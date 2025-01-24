96290f4e1468049d6544836a393f3ef5fcaf268a
shisir <shisirhumagain0@gmail.com>
2025-01-24 16:36:55 -0600

    files modified

---
+++ src/main/java/com/kubernetes/eks/configuration/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.kubernetes.eks.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //Spring Security Provides Login page when you hit your endpoint
+    //by default, to remove below method is required!!!
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());// Allow access to all endpoints without authentication
+        return http.build();
+    }
+}
+
--- src/main/java/com/kubernetes/eks/controller/HelloController.java
+++ src/main/java/com/kubernetes/eks/controller/HelloController.java
@@ -1,10 +1,33 @@
 package com.kubernetes.eks.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kubernetes.eks.entity.Item;
+import com.kubernetes.eks.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/items")
 public class HelloController {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @PostMapping
+    public ResponseEntity<Item> createItem(@RequestBody Item item){
+        Item savedItem = itemRepository.save(item);
+        return new ResponseEntity<>(savedItem, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getItem(@PathVariable Long id){
+        return itemRepository.findById(id)
+                .map(item -> new ResponseEntity<>(item, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello from AWS EKS!";
